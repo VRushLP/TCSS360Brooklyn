cec082b6184e3559ded7784406de5f3d4fcdbf06
VRushLP <rmfarc@uw.edu>
2016-02-08 08:57:02 -0800

    Changes February 8th
    Park is now properly Serializable
    Removed serialVersionID from Park
    The class is going to update again before that becomes useful, so it's
    removed for now as a reminder for when the class is done.
    
    Job is now properly Serializable
    Job now contains the stub for the equals(Object o) method.

--- src/Park.java
+++ src/Park.java
@@ -2,12 +2,9 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class Park
+public class Park implements Serializable
 {
-    /**
-     * 
-     */
-    private static final long serialVersionUID = -6827992796722676364L;
+
     String parkName;
     ParkManager parkManager;
     Collection<Job> parksJobList;
