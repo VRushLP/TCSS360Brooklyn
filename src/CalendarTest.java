639e6dd591a20f707f436fa66a7bec27ce02a817
Duong <dnguyen8@uw.edu>
2016-02-09 10:31:00 -0800

    CalendarTest updated

--- src/CalendarTest.java
+++ src/CalendarTest.java
@@ -13,27 +13,28 @@ public class CalendarTest
     @BeforeClass
     public static void setUpBeforeClass() throws Exception
     {
-    }
-
-    @AfterClass
-    public static void tearDownAfterClass() throws Exception
-    {
-    }
-
-    @Before
-    public void setUp() throws Exception
-    {
-    }
-
-    @After
-    public void tearDown() throws Exception
-    {
+        
     }
 
     @Test
-    public void test()
+    public void testListCurrentJobs()
     {
         fail("Not yet implemented");
     }
-
+    
+    public void testAddJobWhenMax30Jobs()
+    {
+        fail("Not yet implemented");
+    }
+    
+    public void testAddJobsWhenMaxWeeklyJobs()
+    {
+        fail("Not yet implemented");
+    }
+    
+    public void testMaxDateFromToday()
+    {
+        fail("Not yet implemented");
+    }
+    
 }
