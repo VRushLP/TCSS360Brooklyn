9ac91c689c3875846f41e9b04490bb740a33c2a8
VRushLP <rmfarc@uw.edu>
2016-02-12 13:22:59 -0800

    Volunteer edits

--- src/Volunteer.java
+++ src/Volunteer.java
@@ -1,14 +1,31 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 public class Volunteer extends AbstractUser
 {
-    private static final long serialVersionUID = 1426923419814220179L;
+    private static final long serialVersionUID = 5475286744817127535L;
+    private Collection<Job> myJobs;
 
     public Volunteer(String theEmail)
     {
         super(theEmail);
+        myJobs = new ArrayList<>();
     }
 
     public Volunteer(String theEmail, String theFirstName, String theLastName)
     {
         super(theEmail, theFirstName, theLastName);
     }
+
+    public boolean volunteerForJob(Job theJob)
+    {
+        return myJobs.add(theJob);
+    }
+
+    public Collection<Job> getVolunteeredForJobs()
+    {
+        return Collections.unmodifiableCollection(myJobs);
+    }
+
 }
