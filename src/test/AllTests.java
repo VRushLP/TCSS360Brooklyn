092bf1dc3550be6376c6edabf775575cb3d5bfb4
VRushLP <rmfarc@uw.edu>
2016-03-04 11:59:01 -0800

    Corrected an error in AllTests.java

--- src/test/AllTests.java
+++ src/test/AllTests.java
@@ -5,8 +5,8 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ JobTest.class, ParkTest.class, UrbanParkCalendarTest.class,
-        VolunteerTest.class })
+@SuiteClasses({ JobTest.class, ParkTest.class, ParkManagerTest.class,
+        UrbanParkCalendarTest.class, VolunteerTest.class })
 public class AllTests
 {
 }
